947b613c3fab11de1e43d73d0a447919d9da4e29
emirmoretti <emir.moretti@hotmail.com.ar>
2022-06-25 02:24:31 -0300

    postmapping register

--- src/main/java/com/alkemy/challenge/disneyapi/controllers/AuthController.java
+++ src/main/java/com/alkemy/challenge/disneyapi/controllers/AuthController.java
@@ -1,6 +1,11 @@
 package com.alkemy.challenge.disneyapi.controllers;
 
 import com.alkemy.challenge.disneyapi.dto.LoginDTO;
+import com.alkemy.challenge.disneyapi.dto.RegistroDTO;
+import com.alkemy.challenge.disneyapi.entity.Role;
+import com.alkemy.challenge.disneyapi.entity.Usuario;
+import com.alkemy.challenge.disneyapi.repo.RoleRepo;
+import com.alkemy.challenge.disneyapi.repo.UsuarioRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +13,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,12 +28,41 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UsuarioRepo usuarioRepo;
+
+    @Autowired
+    private RoleRepo roleRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/login")
     public ResponseEntity<String> authenticateUser(@RequestBody LoginDTO loginDTO){
         Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUsernameOrEmail(), loginDTO.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return new ResponseEntity<>("Ha iniciado sesion con exito !", HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody RegistroDTO registroDTO){
+        if(usuarioRepo.existsByUsername(registroDTO.getUsername())){
+            return new ResponseEntity<>("Usuario ya existente", HttpStatus.BAD_REQUEST);
+        }
+        if(usuarioRepo.existsByEmail(registroDTO.getEmail())){
+            return new ResponseEntity<>("Usuario ya existente", HttpStatus.BAD_REQUEST);
+        }
+        Usuario usuario = new Usuario();
+        usuario.setUsername(registroDTO.getUsername());
+        usuario.setEmail(registroDTO.getEmail());
+        usuario.setPassword(passwordEncoder.encode(registroDTO.getPassword()));
+
+        Role roles = roleRepo.findByNombre("ROLE_ADMIN").get();
+        usuario.setRoles(Collections.singleton(roles));
+
+        usuarioRepo.save(usuario);
+        return new ResponseEntity<>("usuario creado", HttpStatus.OK);
     }
 
 }
